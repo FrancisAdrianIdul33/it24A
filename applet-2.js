a5691e524024a173f72c9720ddf67893ceb3fa4d
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2024-10-04 22:13:43 +0800

    Added new new coordinates in applet-2.js

--- applet-2.js
+++ applet-2.js
@@ -9,7 +9,7 @@ class LeafletMap {
     initTileLayer() {
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Francis Adrian Idul | Sample of must visit areas around NBSC '
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Francis Adrian Idul | Sample of must visit eatery around NBSC vicinity'
         }).addTo(this.map);
     }
 
