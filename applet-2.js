9769769ee26aa4edc4e240be61fc80f3c983393e
FrancisAdrianIdul33 <20231685@nbsc.edu.ph>
2024-10-09 09:00:58 +0800

    code optimized 2

--- applet-2.js
+++ applet-2.js
@@ -1,84 +1,85 @@
 class LeafletMap {
+
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
 
-        this.attendanceCountBukels = 0;
-        this.attendanceCountWako = 0;
-        this.attendanceCountKNN = 0;
-
-        this.markerCounts = {};
-        this.markers = [];
+        this.attendanceCountB = 0;
+        this.attendanceCountW = 0;
+        this.attendanceCountK = 0;
 
         this.loggedData = [];
 
-        this.btn1 = document.getElementById('btn1');
-        this.btn2 = document.getElementById('btn2');
-        this.btn3 = document.getElementById('btn3');
-        this.btnclear = document.getElementById('btnclear');
+        this.btn = document.getElementById('btn1');
+        this.btn1 = document.getElementById('btn2');
+        this.btn2 = document.getElementById('btn3');
 
+        this.btnclear = document.getElementById('btnclear');
         this.logCountElement = document.getElementById('logCountBukels');
-        this.logCount1Element = document.getElementById('logCountWako');
+        this.logCount1Element = document.getElementById('logCountWWE');
         this.logCount2Element = document.getElementById('logCountKNN');
         this.idContainer = document.getElementById('logContainer');
 
-        // Changed 'this.btn' to 'this.btn3' to avoid reference error
-        this.btn3.addEventListener('click', () => this.dataBukels());
-        this.btn1.addEventListener('click', () => this.dataWako());
-        this.btn2.addEventListener('click', () => this.dataKNN());
+        this.btn.addEventListener('click', () => this.dataB());
+        this.btn1.addEventListener('click', () => this.dataW());
+        this.btn2.addEventListener('click', () => this.dataK());
         this.btnclear.addEventListener('click', () => this.clearLogs());
     }
 
     initTileLayer() {
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 17,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Francis Adrian Idul | Sample of must visit eatery around NBSC vicinity'
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> Francis Adrian Idul | Must Visit Eatery Around NBSC Vicinity'
         }).addTo(this.map);
     }
 
     addMarker(lat, long, message) {
         const marker = L.marker([lat, long]).addTo(this.map)
             .bindPopup(message);
-        this.markers.push(marker); 
     }
 
     loadMarkersFromJson(url) {
         fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`); 
-                }
-                return response.json();
-            })
+            .then(response => response.json())
             .then(data => {
                 data.forEach(marker => {
                     this.addMarker(marker.latitude, marker.longitude, marker.message);
                 });
             })
-            .catch(error => console.error("Error loading markers from JSON:", error));
+            .catch(error => console.error("Error Loading servers:", error));
+    }
+
+
+    clearLogs() {
+        this.attendanceCountB = 0;
+        this.attendanceCountW = 0;
+        this.attendanceCountK = 0;
+
+        this.loggedData = [];
+        this.updateLogDisplay();
     }
 
     displayLogCount() {
-        this.logCountElement.innerHTML = `Bukels Attendance: ${this.attendanceCountBukels}`;
-        this.logCount1Element.innerHTML = `Wako Attendance: ${this.attendanceCountWako}`;
-        this.logCount2Element.innerHTML = `KNN Laboratory Attendance: ${this.attendanceCountKNN}`;
+        this.logCountElement.innerHTML = `SC Building Attendance: ${this.attendanceCountB}`;
+        this.logCount1Element.innerHTML = `BA Building Attendance: ${this.attendanceCountW}`;
+        this.logCount2Element.innerHTML = `CCS Laboratory Attendance: ${this.attendanceCountK}`;
     }
 
-    dataBukels() {
+    dataB() {
         this.addMarker(8.361655, 124.867307, "Bukel's Cafeteria, Store & Print Shop");
-        this.attendanceCountBukels++;
+        this.attendanceCountB++;
         this.updateLogDisplay();
     }
 
-    dataWako() {
+    dataK() {
         this.addMarker(8.362302, 124.867640, 'Wako Wako Eatery');
-        this.attendanceCountWako++;
+        this.attendanceCountW++;
         this.updateLogDisplay();
     }
 
-    dataKNN() {
-        this.addMarker(8.360823, 124.866330, 'Kalenderya ni Nanay');
-        this.attendanceCountKNN++;
+    dataW() {
+        this.addMarker(8.360823, 124.866330, 'Karenderya ni Nanay');
+        this.attendanceCountK++;
         this.updateLogDisplay();
     }
 
@@ -87,35 +88,15 @@ class LeafletMap {
         this.loggedData.forEach(data => {
             const logItem = document.createElement('div');
             logItem.className = 'log-item';
-            logItem.innerText = data; 
             this.idContainer.appendChild(logItem);
         });
         this.displayLogCount();
     }
 
-    clearLogs() {
-        this.attendanceCountBukels = 0;
-        this.attendanceCountWako = 0;
-        this.attendanceCountKNN = 0;
-
-        this.loggedData = [];
-        this.markerCounts = {};
-        this.markers.forEach(marker => {
-            const message = marker.getPopup().getContent().split('<br>')[0];
-            this.markerCounts[message] = 0;
-            this.updateMarkerPopup(marker, message); 
-        });
-
-        this.updateLogDisplay();
-    }
-
-    updateMarkerPopup(marker, message) {
-        
-        marker.getPopup().setContent(`${message} - Count: ${this.markerCounts[message]}`);
-    }
 }
-
 const Mymap = new LeafletMap('map', [8.360697, 124.867345], 17);
+
+
 Mymap.loadMarkersFromJson('applet-2.json');
 
 document.addEventListener('DOMContentLoaded', () => {
